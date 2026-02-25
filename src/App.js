d9fe3dc218aa653ba0139d023abc3fd65e1adc7c
robertogomesbr <robertogomes0277@gmail.com>
2026-02-25 02:34:39 -0300

    enviando conteúdo C08

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Segment } from 'semantic-ui-react';
+import "./App.css";
+import Home from './views/home/Home';
+import FormCliente from './views/cliente/FormCliente';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <FormCliente />
+
+      <div style={{marginTop: '6%'}}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2026 - Projeto WEB IV - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/views/home/Home.jsx
@@ -0,0 +1,29 @@
+import { Container, Grid, Image } from 'semantic-ui-react';
+
+export default function Home () {
+
+   return(
+       <div>
+
+           <div style={{marginTop: '5%'}}>
+               <Container>
+                   <Grid columns={2} divided>
+                       <Grid.Row>
+                           <Grid.Column>
+                               <Image src='/logo-IFPE.png' size='large' />
+                           </Grid.Column>
+                           <Grid.Column>
+                            
+                               Bem vindo ao sistema <strong>OxeFood</strong> ! <br/>
+                               Este sistema foi desenvolvido na disciplina de Desenvolvimento para WEB III. <br/> <br/>
+                               Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/robertogomesbr/oxefood-web-roberto' target='_blank'> https://github.com/robertogomesbr/oxefood-web-roberto </a> <br/> <br/>
+                               Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/robertogomesbr/oxefood-web-roberto' target='_blank'> https://github.com/robertogomesbr/oxefood-web-roberto </a>
+
+                           </Grid.Column>
+                       </Grid.Row>
+                   </Grid>
+               </Container>
+           </div>
+       </div>
+   )
+}
